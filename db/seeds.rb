51cb937383b0a35f973594ddf666cdf5a410ae37
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-07-21 02:41:36 +0900

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do |n|
+
+  # User datas
+  email = Faker::Internet.email
+  name = Faker::Name.name
+  password = 'password'
+  uid = SecureRandom.uuid
+  avatar_url = Faker::Avatar.image
+
+  user = User.create!(email: email,
+                      name: name,
+                      password: password,
+                      password_confirmation: password,
+                      avatar: open(avatar_url),
+                      uid: uid)
+  # Article datas
+  picture_url = Faker::Avatar.image
+
+  Article.create!(title: 'title' + n.to_s,
+                 content: 'comment' + n.to_s,
+                 picture: open(picture_url),
+                 user_id: user.id)
+end

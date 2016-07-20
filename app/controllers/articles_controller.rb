d584949ac5a1783cf39874d7b7f2d9ef16c0051e
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-07-21 01:24:27 +0900

    add validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,17 +2,23 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:destroy, :update]
 
   def create
-    article = Article.new(article_params)
-    article.user_id = current_user.id
-    article.save
-
-    redirect_to :root, notice: '記事を作成しました'
+    @article = Article.new(article_params)
+    @article.user_id = current_user.id
+    if @article.save
+      redirect_to :root, notice: '記事を作成しました'
+    else
+      render template: 'top/index'
+    end
   end
 
   def update
-    @article.update(article_params)
-
-    redirect_to :root, notice: '記事を更新しました'
+    if @article.update(article_params)
+      redirect_to :root, notice: '記事を更新しました'
+    else
+      respond_to do |format|
+        format.json { render json: @article.errors.full_messages, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,5 +2,6 @@ class TopController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @article = Article.new
   end
 end

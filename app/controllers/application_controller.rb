36e4991c7c2e2ec789dfc93a4eb2987b0e930c54
Makoto Kitamura <today.is.sailingday24@gmail.com>
2016-07-20 22:33:10 +0900

    add function to login by social account.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  PERMISSIBLE_ATTRIBUTES = [:name]
+  PERMISSIBLE_ATTRIBUTES = [:name, :avatar, :avatar_cache]
+
 
   private
 
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,14 @@
 module ApplicationHelper
 
-  def profile_img(user)
-    return image_tag(user.avatar, alt: user.name) if user.avatar?
+  def profile_img(user, options = {})
+    options.store :alt, user.name
+    return image_tag(user.avatar, options) if user.avatar?
 
     unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'silhouette-human-256.png'
     end
-    image_tag(img_url, alt: user.name)
+    image_tag(img_url, options)
   end
 end
